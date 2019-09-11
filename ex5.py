f0163244c59f610f72e420f571cced93e7fe71db
Miguel Martinez <mmartinez@oc6438801781.ibm.com>
2019-09-11 09:11:31 -0400

    Commit #1

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Miguel A. Martinez'
+my_age = 41 # Not a lie
+my_height = 172 # cm
+my_weight = 100 # Kilos
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print("Let's talk about {my_name}.")
+print("He's {my_height} cms tall.")
+print("He's {my_weight} Kilos heavy")
+print("Actually that's not too heavy.")
+print("He's got {my_eyes} eyes and {my_hair} hair.")
+print("His theeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_weight + my_height
+print("If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
